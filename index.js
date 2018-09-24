d90a40add46faa2a258dc728d0c770b7a0914bb8
qsb3008 <qsb3008@163.com>
2018-09-24 15:55:25 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const request = require('request');
+
+// request({
+//   url: 'https://api.douban.com/v2/movie/top250',
+//   json: true
+// }, (error, response, body) => {
+//   console.log(response);
+// })
+
+const greeting = require('./src/greeting')
+
+greeting.hello()
